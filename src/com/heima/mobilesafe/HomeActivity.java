46e5a11b9dfbd08cd60f5ad6c5d19dae667d5077
mlc <282733193@qq.com>
2016-07-14 15:59:25 +0800

    输入密码对话框的业务逻辑实现

--- src/com/heima/mobilesafe/HomeActivity.java
+++ src/com/heima/mobilesafe/HomeActivity.java
@@ -1,26 +1,40 @@
 package com.heima.mobilesafe;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.Intent;
-import android.graphics.drawable.GradientDrawable;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends Activity {
 
 	private GridView gv_home_gridview;
+	private AlertDialog dialog;
+	private EditText et_setpassword_password;
+	private EditText et_setpassword_confrim;
+	private SharedPreferences sp;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		sp = getSharedPreferences("config", MODE_PRIVATE);
 		gv_home_gridview = (GridView) findViewById(R.id.gv_home_gridview);
 		gv_home_gridview.setAdapter(new myAdapter());
 		gv_home_gridview.setOnItemClickListener(new OnItemClickListener() {
@@ -29,7 +43,21 @@ public class HomeActivity extends Activity {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
 				switch (position) {
-				case 8:
+				
+				case 0://手机防盗
+					//跳转到手机防盗模块
+					//判断用户是否是第一次点击，若是则设置密码，不是第一次点击，则输入密码，密码输入正确才能进入该模块
+					//判断用户是否设置过密码
+					if (TextUtils.isEmpty(sp.getString("password", ""))) { 
+						//密码为空，弹出设置密码的对话框
+						showSetPasswordDialog();
+					} else {
+						//密码不为空，弹出输入密码的对话框
+						showEnterPasswordDialog();
+					}
+					
+					break;
+				case 8://设置中心
 					Intent intent = new Intent(getApplicationContext(), SettingActivity.class);
 					startActivity(intent);
 					break;
@@ -40,7 +68,114 @@ public class HomeActivity extends Activity {
 			}
 		});
 	}
-	
+	/**
+	 * 输入密码对话框
+	 */
+	protected void showEnterPasswordDialog() {
+
+		AlertDialog.Builder builder = new Builder(this);
+		//设置对话框不能取消
+		builder.setCancelable(false);
+		//将布局文件转化成view对象
+		View view = View.inflate(getApplicationContext(), R.layout.dialog_enterpassword, null);
+		//获取子布局中的控件
+		final EditText et_enterpassword_password = (EditText) view.findViewById(R.id.et_enterpassword_password);
+		Button btn_ok = (Button) view.findViewById(R.id.btn_ok);
+		Button btn_cancle = (Button) view.findViewById(R.id.btn_cancle);
+		btn_ok.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//1.获取输入密码对话框的密码
+				String password = et_enterpassword_password.getText().toString().trim();
+				//2.判断密码是否为空
+				if (TextUtils.isEmpty(password)) {
+					Toast.makeText(getApplicationContext(), "输入的密码为空，请重新输入！", 0).show();
+					return;
+				}
+				//3.将密码和sp中保存的密码进行判断
+				String sp_password = sp.getString("password", "");
+				if (sp_password.equals(password)) {
+					//页面的跳转
+					
+					//隐藏对话框
+					dialog.dismiss();
+					Toast.makeText(getApplicationContext(), "密码输入正确", 0).show();
+				} else {
+					Toast.makeText(getApplicationContext(), "密码输入不正确，请重新输入!", 0).show();
+				}
+			}
+		});
+		btn_cancle.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//隐藏对话框
+				dialog.dismiss();
+			}
+		});
+		builder.setView(view);
+//		builder.show();
+		
+		dialog = builder.create();
+		dialog.show();
+	}
+	/**
+	 * 设置密码的对话框
+	 */
+	protected void showSetPasswordDialog() {
+		AlertDialog.Builder builder = new Builder(this);
+		//设置对话框不能取消
+		builder.setCancelable(false);
+		//将布局文件转化成view对象
+		View view = View.inflate(getApplicationContext(), R.layout.dialog_setpassword, null);
+		et_setpassword_password = (EditText) view.findViewById(R.id.et_setpassword_password);
+		et_setpassword_confrim = (EditText) view.findViewById(R.id.et_setpassword_confrim);
+		Button btn_ok = (Button) view.findViewById(R.id.btn_ok);
+		Button btn_cancle = (Button) view.findViewById(R.id.btn_cancle);
+		//设置确定和取消按钮的点击事件
+		btn_ok.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//设置密码
+				//1.获取密码输入框输入的内容
+				String password = et_setpassword_password.getText().toString().trim();
+				//2.判断密码是否为空
+				if (TextUtils.isEmpty(password)) {
+					Toast.makeText(getApplicationContext(), "密码为空，请重新输入！", 0).show();
+					return;
+				}
+				//3.获取确认密码
+				String confrim = et_setpassword_confrim.getText().toString().trim();
+				//4.判断两次密码是否一致;密码一致就保存到sharedPreference
+				if (password.equals(confrim)) {
+					Editor edit = sp.edit();
+					edit.putString("password", password);
+					edit.commit();
+					//取消对话框
+					dialog.dismiss();
+					Toast.makeText(getApplicationContext(), "密码设置成功", 0).show();
+				} else {
+					Toast.makeText(getApplicationContext(), "两次密码设置不一致", 0).show();
+				}
+			}
+		});
+		btn_cancle.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//隐藏对话框
+				dialog.dismiss();
+			}
+		}); 
+		builder.setView(view);
+//		builder.show();
+		
+		dialog = builder.create();
+		dialog.show();
+	}
+
 	public class myAdapter extends BaseAdapter{
 		//数据源
 		int[] imageId = { R.drawable.safe, R.drawable.callmsgsafe, R.drawable.app,
