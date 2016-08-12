5fbbae64e7c93bc9260413a19ec8d5184744dd41
mlc <282733193@qq.com>
2016-08-12 22:43:50 +0800

    实现程序的解锁操作

--- src/com/heima/mobilesafe/WatchDogActivity.java
+++ src/com/heima/mobilesafe/WatchDogActivity.java
@@ -9,13 +9,17 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class WatchDogActivity extends Activity {
 
 	private ImageView iv_watchdog_icon;
 	private TextView tv_watchdog_name;
+	private EditText et_watchdog_password;
+	private String packageName;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +28,11 @@ public class WatchDogActivity extends Activity {
 		
 		iv_watchdog_icon = (ImageView) findViewById(R.id.iv_watchdog_icon);
 		tv_watchdog_name = (TextView) findViewById(R.id.tv_watchdog_name);
+		et_watchdog_password = (EditText) findViewById(R.id.et_watchdog_password);
 		
 		//接收获取到的数据
 		Intent intent = getIntent();
-		String packageName = intent.getStringExtra("packageName");
+		packageName = intent.getStringExtra("packageName");
 		//获取包名的管理者
 		PackageManager pm = getPackageManager();
 		try {
@@ -65,5 +70,22 @@ public class WatchDogActivity extends Activity {
 			finish();
 		}
 		return super.onKeyDown(keyCode, event);
+	}
+	//按钮的解锁操作
+	public void lock(View v){
+		String password = et_watchdog_password.getText().toString();
+		if ("123".equals(password)) {
+			//解锁
+			//一般是通过发送广播形式将解锁应用的信息给服务
+			Intent intent = new Intent();
+			//发送自定义广播
+			intent.setAction("com.heima.mobilesafe.unlock");
+			intent.putExtra("packageName", packageName);
+			sendBroadcast(intent);
+			finish();
+		}else {
+			//提示用户
+			Toast.makeText(getApplicationContext(), "密码错误，请重新输入！！", 0).show();
+		}
 	}
 }
--- src/com/heima/mobilesafe/service/WatchdogService.java
+++ src/com/heima/mobilesafe/service/WatchdogService.java
@@ -8,8 +8,11 @@ import com.heima.mobilesafe.db.dao.WatchDogDao;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningTaskInfo;
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.os.SystemClock;
 
@@ -17,14 +20,53 @@ public class WatchdogService extends Service {
 
 	private WatchDogDao watchDogDao;
 	private boolean islock;
+	private UnLockReceiver unLockReceiver;
+	private String unlockpackagename;
+	private ScreenReceiver screenReceiver;
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
 	}
-	
+	/**
+	 * 自定义解锁的广播接收者
+	 * @author lenovo
+	 *
+	 */
+	public class UnLockReceiver extends BroadcastReceiver{
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			//解锁操作
+			unlockpackagename = intent.getStringExtra("packageName");
+		}
+		
+	}
+	//屏幕锁屏的广播接收者
+	public class ScreenReceiver extends BroadcastReceiver{
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			//屏幕解锁操作，使应用重新加锁
+			unlockpackagename=null;
+		}
+		
+	}
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		//注册解锁的广播接收者
+		unLockReceiver = new UnLockReceiver();
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction("com.heima.mobilesafe.unlock");
+		registerReceiver(unLockReceiver, intentFilter);
+		
+		//注册屏幕锁屏的广播接收者
+		screenReceiver = new ScreenReceiver();
+		IntentFilter screenOffintentFilter = new IntentFilter();
+		screenOffintentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+		registerReceiver(screenReceiver, screenOffintentFilter);
+		
 		islock = true;
 		watchDogDao = new WatchDogDao(getApplicationContext());
 		//时时刻刻监听用户打开的程序
@@ -47,11 +89,13 @@ public class WatchdogService extends Service {
 						System.out.println(packageName);
 						//通过查询数据库，如果数据库中有该包名，则跳转到解锁界面，否则不进行跳转
 						if (watchDogDao.queryLockApp(packageName)){
-							Intent intent = new Intent(WatchdogService.this, WatchDogActivity.class);
-							intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-							//将加锁的应用程序包名传递给解锁界面
-							intent.putExtra("packageName", packageName);
-							startActivity(intent);
+							if (!packageName.equals(unlockpackagename)) {
+								Intent intent = new Intent(WatchdogService.this, WatchDogActivity.class);
+								intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+								//将加锁的应用程序包名传递给解锁界面
+								intent.putExtra("packageName", packageName);
+								startActivity(intent);
+							}
 						}
 					}
 					SystemClock.sleep(300);
@@ -65,5 +109,17 @@ public class WatchdogService extends Service {
 		super.onDestroy();
 		//当服务关闭时，禁止监听用户打开的应用程序
 		islock = false;
+		//注销广播接收者
+		if (unLockReceiver !=null) {
+			unregisterReceiver(unLockReceiver);
+			unLockReceiver =null;
+		}
+		
+		//注销屏幕锁屏的广播接收者
+		if (screenReceiver !=null) {
+			unregisterReceiver(screenReceiver);
+			screenReceiver =null;
+		}
+		
 	}
 }
